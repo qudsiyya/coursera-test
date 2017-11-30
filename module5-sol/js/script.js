acc0cc5fd7940e3067660ddc7e1874c17b59a98f
qudsiyya <qudsiyya@gmail.com>
2017-11-30 12:11:59 +0400

    Update script.js

--- module5-sol/js/script.js
+++ module5-sol/js/script.js
@@ -114,7 +114,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      chosenCategoryShortName = "'" + chosenCategoryShortName.short_name + "'";
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "chosenCategoryShortName", chosenCategoryShortName);
        
       //var homeHtmlToInsertIntoMainPage = 
