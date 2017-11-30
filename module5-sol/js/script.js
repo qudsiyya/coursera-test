ba01de1dd59fdf66538196199f804c3fa7e531e0
qudsiyya <qudsiyya@gmail.com>
2017-11-30 12:50:36 +0400

    Update script.js

--- module5-sol/js/script.js
+++ module5-sol/js/script.js
@@ -98,7 +98,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;     
-      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+      //chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "chosenCategoryShortName", chosenCategoryShortName);
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
