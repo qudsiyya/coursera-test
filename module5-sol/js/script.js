1ba06ea3f303e1589fe0da6a56d4d621b47d8b84
qudsiyya <qudsiyya@gmail.com>
2017-11-30 12:33:34 +0400

    Update script.js

--- module5-sol/js/script.js
+++ module5-sol/js/script.js
@@ -100,9 +100,9 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories);
-      var temp = chosenCategoryShortName.short_name;
-      console.log("after step 2:", temp);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      //var temp = chosenCategoryShortName.short_name;
+      console.log("after step 2:", chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -115,9 +115,9 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      temp = "'" + temp + "'";
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "temp", temp);
-       console.log("after step 3:", temp);
+      //temp = "'" + temp + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "chosenCategoryShortName", chosenCategoryShortName);
+       //console.log("after step 3:", temp);
       //var homeHtmlToInsertIntoMainPage = 
 
 
